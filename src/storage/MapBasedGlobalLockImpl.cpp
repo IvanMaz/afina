de1becbdd120b9780a436b2a1552b56edb1e278f
IvanMaz <ivanmazharov@gmail.com>
2018-03-03 03:07:22 +0300

    Implemented double linked list

--- src/storage/MapBasedGlobalLockImpl.cpp
+++ src/storage/MapBasedGlobalLockImpl.cpp
@@ -6,97 +6,166 @@ namespace Afina {
 namespace Backend {
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) { 
+bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) {
     std::unique_lock<std::mutex> guard(_lock);
     std::string tmp_value;
-    if (get_value_if_exists(key, tmp_value)){
-        _list.front().second = value;
+    if (get_value_if_exists(key, tmp_value)) {
+        _list->front()->value = value;
     } else {
         auto new_elem_size = key.size() + value.size();
-        if (new_elem_size > _max_size){
+        if (new_elem_size > _max_size) {
             return false;
         }
-        while (new_elem_size + _size > _max_size){
-            auto last = _list.back();
-            _size -= (last.first.size() + last.second.size());
-            _backend.erase(_list.back().first);
-            _list.pop_back();
+        while (new_elem_size + _size > _max_size) {
+            auto last = _list->back();
+            _size -= (last->key.size() + last->key.size());
+            _backend.erase(_list->back()->key);
+            _list->pop_back();
         }
-        _list.push_front(std::pair<std::string, std::string>(key, value));
-        _backend.emplace(_list.front().first, _list.begin());
+        _list->push_front(key, value);
+        _backend.emplace(_list->front()->key, _list->front());
         _size += new_elem_size;
     }
-    return true; 
+    return true;
 }
 
 // See MapBasedGlobalLockImpl.h
 bool MapBasedGlobalLockImpl::PutIfAbsent(const std::string &key, const std::string &value) {
     std::unique_lock<std::mutex> guard(_lock);
     std::string tmp_value;
-    if (get_value_if_exists(key, tmp_value)){
+    if (get_value_if_exists(key, tmp_value)) {
         return false;
     } else {
         auto new_elem_size = key.size() + value.size();
-        if (new_elem_size > _max_size){
+        if (new_elem_size > _max_size) {
             return false;
         }
-        while (new_elem_size + _size > _max_size){
-            auto last = _list.back();
-            _size -= (last.first.size() + last.second.size());
-            _backend.erase(_list.back().first);
-            _list.pop_back();
+        while (new_elem_size + _size > _max_size) {
+            auto last = _list->back();
+            _size -= (last->key.size() + last->key.size());
+            _backend.erase(_list->back()->key);
+            _list->pop_back();
         }
-        _list.push_front(std::pair<std::string, std::string>(key, value));
-        _backend[key] = _list.begin();
+        _list->push_front(key, value);
+        _backend[key] = _list->front();
         _size += new_elem_size;
     }
-    return true; 
+    return true;
 }
 
 // See MapBasedGlobalLockImpl.h
 bool MapBasedGlobalLockImpl::Set(const std::string &key, const std::string &value) {
     std::unique_lock<std::mutex> guard(_lock);
     std::string tmp_value;
-    if (get_value_if_exists(key, tmp_value)){
-        _list.front().second = value;
+    if (get_value_if_exists(key, tmp_value)) {
+        _list->front()->value = value;
     }
-    return true; 
+    return true;
 }
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Delete(const std::string &key) { 
+bool MapBasedGlobalLockImpl::Delete(const std::string &key) {
     std::unique_lock<std::mutex> guard(_lock);
     std::string tmp_value;
-    if (get_value_if_exists(key, tmp_value)){
+    if (get_value_if_exists(key, tmp_value)) {
         auto item = _backend.find(key);
-        _list.erase(item->second);
+        _list->erase(item->second);
         _backend.erase(key);
         return true;
     }
-    return false; 
+    return false;
 }
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Get(const std::string &key, std::string &value) const { 
+bool MapBasedGlobalLockImpl::Get(const std::string &key, std::string &value) const {
     std::unique_lock<std::mutex> guard(_lock);
     auto it = _backend.find(key);
-    if (it != _backend.end()){
-        _list.splice(_list.begin(), _list, it->second);
-        value = _list.front().second;
+    if (it != _backend.end()) {
+        _list->move_to_front(it->second);
+        value = _list->front()->value;
         return true;
     }
-    return false; 
+    return false;
 }
 
-bool MapBasedGlobalLockImpl::get_value_if_exists(const std::string &key, std::string &value) { 
+bool MapBasedGlobalLockImpl::get_value_if_exists(const std::string &key, std::string &value) {
     auto it = _backend.find(key);
-    if (it != _backend.end()){
-        _list.splice(_list.begin(), _list, it->second);
-        value = _list.front().second;
+    if (it != _backend.end()) {
+        _list->move_to_front(it->second);
+        value = _list->front()->value;
         return true;
     }
-    return false; 
+    return false;
 }
+
+Dl_list::Dl_list() { head = NULL; }
+
+Dl_list::~Dl_list() {
+    while (head) {
+        Node *tmp = head;
+        head = head->next;
+        delete (tmp);
+    }
+}
+
+void Dl_list::push_front(std::string key, std::string value) {
+
+    Node *node = new Node();
+    node->key = key;
+    node->value = value;
+    node->prev = NULL;
+
+    if (head == NULL) {
+        node->next = NULL;
+        head = node;
+        tail = node;
+    } else {
+        node->next = head;
+        node->next->prev = node;
+        head = node;
+    }
+}
+
+void Dl_list::pop_back() {
+    Node *tmp = tail;
+    tail->prev->next = NULL;
+    tail = tail->prev;
+    delete (tmp);
+}
+
+void Dl_list::erase(Node *node) {
+    if (node == head) {
+        Node *tmp = head;
+        head = node->next;
+        head->prev = NULL;
+        delete (tmp);
+    } else if (node == tail) {
+        pop_back();
+    } else {
+        node->prev->next = node->next;
+        delete (node);
+    }
+}
+
+void Dl_list::move_to_front(Node *node) {
+    if (node != head) {
+        if (node == tail) {
+            tail = node->prev;
+        } else {
+            node->next->prev = node->prev;
+        }
+        node->prev->next = node->next;
+        node->prev = NULL;
+        head->prev = node;
+        node->next = head;
+        head = node;
+    }
+}
+
+Node *Dl_list::front() { return head; }
+
+Node *Dl_list::back() { return tail; }
+
 
 } // namespace Backend
 } // namespace Afina
--- src/storage/MapBasedGlobalLockImpl.h
+++ src/storage/MapBasedGlobalLockImpl.h
@@ -1,11 +1,11 @@
 #ifndef AFINA_STORAGE_MAP_BASED_GLOBAL_LOCK_IMPL_H
 #define AFINA_STORAGE_MAP_BASED_GLOBAL_LOCK_IMPL_H
 
+#include <functional>
+#include <list>
 #include <map>
 #include <mutex>
 #include <string>
-#include <list>
-#include <functional>
 
 #include "../../include/afina/Storage.h"
 
@@ -17,10 +17,35 @@ namespace Backend {
  *
  *
  */
+class Node {
+public:
+    std::string key;
+    std::string value;
+    Node *next;
+    Node *prev;
+};
+
+class Dl_list {
+public:
+    Dl_list();
+    ~Dl_list();
+    void push_front(std::string, std::string);
+    void pop_back();
+    void erase(Node *);
+    void move_to_front(Node *);
+    void print();
+    Node *front();
+    Node *back();
+
+private:
+    Node *head;
+    Node *tail;
+};
+
 class MapBasedGlobalLockImpl : public Afina::Storage {
 public:
-    MapBasedGlobalLockImpl(size_t max_size = 1024) : _max_size(max_size) , _size(0){}
-    ~MapBasedGlobalLockImpl() {}
+    MapBasedGlobalLockImpl(size_t max_size = 1024) : _max_size(max_size), _size(0), _list(new Dl_list()) {}
+    ~MapBasedGlobalLockImpl() {delete(_list);}
 
     // Implements Afina::Storage interface
     bool Put(const std::string &key, const std::string &value) override;
@@ -41,11 +66,13 @@ private:
     size_t _max_size;
     size_t _size;
     mutable std::mutex _lock;
-    mutable std::list<std::pair<std::string, std::string>> _list;
-    std::map<std::reference_wrapper<const std::string>, decltype(_list)::iterator, std::less<const std::string>> _backend;
+    //mutable std::list<std::pair<std::string, std::string>> _list;
+    //std::map<std::reference_wrapper<const std::string>, decltype(_list)::iterator, std::less<const std::string>>_backend;
+    
+    Dl_list *_list;
+    std::map<std::reference_wrapper<const std::string>, Node *, std::less<const std::string>>_backend;
 
     bool get_value_if_exists(const std::string &key, std::string &value);
-
 };
 
 } // namespace Backend
